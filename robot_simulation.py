6f97abf2dbd06b8f8d5339ce79d7d8622e170aeb
Jeralyn123 <43395674+Jeralyn123@users.noreply.github.com>
2022-03-22 10:52:27 +0800

    Add files via upload

---
+++ robot_simulation.py
@@ -0,0 +1,344 @@
+from scipy import spatial
+from skimage import io
+import numpy as np
+import numpy.ma as ma
+import time
+import sys
+from scipy import ndimage
+import matplotlib.pyplot as plt
+
+sys.path.append(sys.path[0] + '/..')
+from build.inverse_sensor_model import *
+from build.astar import *
+from random import shuffle
+import os
+
+
+class Robot:
+    def __init__(self, index_map, train, plot):
+        self.mode = train
+        self.plot = plot
+        if self.mode:
+            self.map_dir = '../DungeonMaps/train'
+        else:
+            self.map_dir = '../DungeonMaps/test'
+        self.map_list = os.listdir(self.map_dir)
+        self.map_number = np.size(self.map_list)
+        if self.mode:
+            shuffle(self.map_list)
+        self.li_map = index_map
+        self.global_map, self.robot_position = self.map_setup(self.map_dir + '/' + self.map_list[self.li_map])
+        self.op_map = np.ones(self.global_map.shape) * 127
+        self.map_size = np.shape(self.global_map)
+        self.finish_percent = 0.985
+        self.resolution = 1
+        self.sensor_range = 80
+        self.old_position = np.zeros([2])
+        self.old_op_map = np.empty([0])
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        self.action_space = np.genfromtxt(current_dir + '/action_points.csv', delimiter=",")
+        self.t = self.map_points(self.global_map)
+        self.free_tree = spatial.KDTree(self.free_points(self.global_map).tolist())
+        self.robot_size = 6
+        self.local_size = 40
+        if self.plot:
+            self.xPoint = np.array([self.robot_position[0]])
+            self.yPoint = np.array([self.robot_position[1]])
+            self.x2frontier = np.empty([0])
+            self.y2frontier = np.empty([0])
+
+    def begin(self):
+        self.op_map = self.inverse_sensor(self.robot_position, self.sensor_range, self.op_map, self.global_map)
+        step_map = self.robot_model(self.robot_position, self.robot_size, self.t, self.op_map)
+        map_local = self.local_map(self.robot_position, step_map, self.map_size, self.sensor_range + self.local_size)
+        if self.plot:
+            self.plot_env()
+        return map_local
+
+    def step(self, action_index):
+        terminal = False
+        complete = False
+        new_location = False
+        all_map = False
+        self.old_position = self.robot_position.copy()
+        self.old_op_map = self.op_map.copy()
+
+        # take action
+        self.take_action(action_index, self.robot_position)
+
+        # collision check
+        collision_points, collision_index = self.collision_check(self.old_position, self.robot_position, self.map_size,
+                                                                 self.global_map)
+
+        if collision_index:
+            self.robot_position = self.nearest_free(self.free_tree, collision_points)
+            self.op_map = self.inverse_sensor(self.robot_position, self.sensor_range, self.op_map, self.global_map)
+            step_map = self.robot_model(self.robot_position, self.robot_size, self.t, self.op_map)
+        else:
+            self.op_map = self.inverse_sensor(self.robot_position, self.sensor_range, self.op_map, self.global_map)
+            step_map = self.robot_model(self.robot_position, self.robot_size, self.t, self.op_map)
+
+        map_local = self.local_map(self.robot_position, step_map, self.map_size, self.sensor_range + self.local_size)
+        reward = self.get_reward(self.old_op_map, self.op_map, collision_index)
+
+        if reward <= 0.02 and not collision_index:
+            reward = -0.8
+            new_location = True
+            terminal = True
+
+        # during training, the robot is relocated if it has a collision
+        # during testing, the robot will use collision check to avoid the collision
+        if collision_index:
+            if not self.mode:
+                new_location = False
+                terminal = False
+            else:
+                new_location = True
+                terminal = True
+            if self.plot and self.mode:
+                self.xPoint = ma.append(self.xPoint, self.robot_position[0])
+                self.yPoint = ma.append(self.yPoint, self.robot_position[1])
+                self.plot_env()
+            self.robot_position = self.old_position.copy()
+            self.op_map = self.old_op_map.copy()
+            if self.plot and self.mode:
+                self.xPoint[self.xPoint.size-1] = ma.masked
+                self.yPoint[self.yPoint.size-1] = ma.masked
+        else:
+            if self.plot:
+                self.xPoint = ma.append(self.xPoint, self.robot_position[0])
+                self.yPoint = ma.append(self.yPoint, self.robot_position[1])
+                self.plot_env()
+
+        # check if exploration is finished
+        if np.size(np.where(self.op_map == 255))/np.size(np.where(self.global_map == 255)) > self.finish_percent:
+            self.li_map += 1
+            if self.li_map == self.map_number:
+                self.li_map = 0
+                all_map = True
+            self.__init__(self.li_map, self.mode, self.plot)
+            complete = True
+            new_location = False
+            terminal = True
+
+        return map_local, reward, terminal, complete, new_location, collision_index, all_map
+
+    def rescuer(self):
+        complete = False
+        all_map = False
+        pre_position = self.robot_position.copy()
+        self.robot_position = self.frontier(self.op_map, self.map_size, self.t)
+        self.op_map = self.inverse_sensor(self.robot_position, self.sensor_range, self.op_map, self.global_map)
+        step_map = self.robot_model(self.robot_position, self.robot_size, self.t, self.op_map)
+        map_local = self.local_map(self.robot_position, step_map, self.map_size, self.sensor_range + self.local_size)
+
+        if self.plot:
+            path = self.astar_path(self.op_map, pre_position.tolist(), self.robot_position.tolist())
+            self.x2frontier = ma.append(self.x2frontier, ma.masked)
+            self.y2frontier = ma.append(self.y2frontier, ma.masked)
+            self.x2frontier = ma.append(self.x2frontier, path[1, :])
+            self.y2frontier = ma.append(self.y2frontier, path[0, :])
+            self.xPoint = ma.append(self.xPoint, ma.masked)
+            self.yPoint = ma.append(self.yPoint, ma.masked)
+            self.xPoint = ma.append(self.xPoint, self.robot_position[0])
+            self.yPoint = ma.append(self.yPoint, self.robot_position[1])
+            self.plot_env()
+
+        if np.size(np.where(self.op_map == 255))/np.size(np.where(self.global_map == 255)) > self.finish_percent:
+            self.li_map += 1
+            if self.li_map == self.map_number:
+                self.li_map = 0
+                all_map = True
+            self.__init__(self.li_map, self.mode, self.plot)
+            complete = True
+            new_location = False
+            terminal = True
+        return map_local, complete, all_map
+
+    def take_action(self, action_index, robot_position):
+        move_action = self.action_space[action_index, :]
+        robot_position[0] = np.round(robot_position[0] + move_action[0])
+        robot_position[1] = np.round(robot_position[1] + move_action[1])
+
+    def map_setup(self, location):
+        global_map = (io.imread(location, 1) * 255).astype(int)
+        robot_location = np.nonzero(global_map == 208)
+        robot_location = np.array([np.array(robot_location)[1, 127], np.array(robot_location)[0, 127]])
+        global_map = (global_map > 150)
+        global_map = global_map * 254 + 1
+        return global_map, robot_location
+
+    def map_points(self, map_glo):
+        map_x = map_glo.shape[1]
+        map_y = map_glo.shape[0]
+        x = np.linspace(0, map_x - 1, map_x)
+        y = np.linspace(0, map_y - 1, map_y)
+        t1, t2 = np.meshgrid(x, y)
+        points = np.vstack([t1.T.ravel(), t2.T.ravel()]).T
+        return points
+
+    def local_map(self, robot_location, map_glo, map_size, local_size):
+        minX = robot_location[0] - local_size
+        maxX = robot_location[0] + local_size
+        minY = robot_location[1] - local_size
+        maxY = robot_location[1] + local_size
+
+        if minX < 0:
+            maxX = abs(minX) + maxX
+            minX = 0
+        if maxX > map_size[1]:
+            minX = minX - (maxX - map_size[1])
+            maxX = map_size[1]
+        if minY < 0:
+            maxY = abs(minY) + maxY
+            minY = 0
+        if maxY > map_size[0]:
+            minY = minY - (maxY - map_size[0])
+            maxY = map_size[0]
+
+        map_loc = map_glo[minY:maxY][:, minX:maxX]
+        return map_loc
+
+    def free_points(self, op_map):
+        index = np.where(op_map == 255)
+        free = np.asarray([index[1], index[0]]).T
+        return free
+
+    def get_reward(self, old_op_map, op_map, coll_index):
+        if not coll_index:
+            reward = float(np.size(np.where(op_map == 255)) - np.size(np.where(old_op_map == 255))) / 14000
+            if reward > 1:
+                reward = 1
+        else:
+            reward = -1
+        return reward
+
+    def nearest_free(self, tree, point):
+        pts = np.atleast_2d(point)
+        index = tuple(tree.query(pts)[1])
+        nearest = tree.data[index]
+        return nearest
+
+    def robot_model(self, position, robot_size, points, map_glo):
+        map_copy = map_glo.copy()
+        robot_points = self.range_search(position, robot_size, points)
+        for i in range(0, robot_points.shape[0]):
+            rob_loc = np.int32(robot_points[i, :])
+            rob_loc = np.flipud(rob_loc)
+            map_copy[tuple(rob_loc)] = 76
+        map_with_robot = map_copy
+        return map_with_robot
+
+    def range_search(self, position, r, points):
+        nvar = position.shape[0]
+        r2 = r ** 2
+        s = 0
+        for d in range(0, nvar):
+            s += (points[:, d] - position[d]) ** 2
+        idx = np.nonzero(s <= r2)
+        idx = np.asarray(idx).ravel()
+        inrange_points = points[idx, :]
+        return inrange_points
+
+    def collision_check(self, start_point, end_point, map_size, map_glo):
+        x0, y0 = start_point.round()
+        x1, y1 = end_point.round()
+        dx, dy = abs(x1 - x0), abs(y1 - y0)
+        x, y = x0, y0
+        error = dx - dy
+        x_inc = 1 if x1 > x0 else -1
+        y_inc = 1 if y1 > y0 else -1
+        dx *= 2
+        dy *= 2
+
+        coll_points = np.ones((1, 2), np.uint8) * -1
+
+        while 0 <= x < map_size[1] and 0 <= y < map_size[0]:
+            k = map_glo.item(y, x)
+            if k == 1:
+                coll_points.itemset((0, 0), x)
+                coll_points.itemset((0, 1), y)
+                break
+
+            if x == end_point[0] and y == end_point[1]:
+                break
+
+            if error > 0:
+                x += x_inc
+                error -= dy
+            else:
+                y += y_inc
+                error += dx
+        if np.sum(coll_points) == -2:
+            coll_index = False
+        else:
+            coll_index = True
+
+        return coll_points, coll_index
+
+    def inverse_sensor(self, robot_position, sensor_range, op_map, map_glo):
+        op_map = inverse_sensor_model(robot_position[0], robot_position[1], sensor_range, op_map, map_glo)
+        return op_map
+
+    def frontier(self, op_map, map_size, points):
+        y_len = map_size[0]
+        x_len = map_size[1]
+        mapping = op_map.copy()
+        # 0-1 unknown area map
+        mapping = (mapping == 127) * 1
+        mapping = np.lib.pad(mapping, ((1, 1), (1, 1)), 'constant', constant_values=0)
+        fro_map = mapping[2:][:, 1:x_len + 1] + mapping[:y_len][:, 1:x_len + 1] + mapping[1:y_len + 1][:, 2:] + \
+                  mapping[1:y_len + 1][:, :x_len] + mapping[:y_len][:, 2:] + mapping[2:][:, :x_len] + mapping[2:][:,
+                                                                                                      2:] + \
+                  mapping[:y_len][:, :x_len]
+        ind_free = np.where(op_map.ravel(order='F') == 255)[0]
+        ind_fron_1 = np.where(1 < fro_map.ravel(order='F'))[0]
+        ind_fron_2 = np.where(fro_map.ravel(order='F') < 8)[0]
+        ind_fron = np.intersect1d(ind_fron_1, ind_fron_2)
+        ind_to = np.intersect1d(ind_free, ind_fron)
+        f = points[ind_to]
+        f = f.astype(int)
+        return f[0]
+
+    def unique_rows(self, a):
+        a = np.ascontiguousarray(a)
+        unique_a = np.unique(a.view([('', a.dtype)] * a.shape[1]))
+        result = unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))
+        result = result[~np.isnan(result).any(axis=1)]
+        return result
+
+    def astar_path(self, weights, start, goal, allow_diagonal=True):
+        temp_start = [start[1], start[0]]
+        temp_goal = [goal[1], goal[0]]
+        temp_weight = (weights < 150) * 254 + 1
+        # For the heuristic to be valid, each move must cost at least 1.
+        if temp_weight.min(axis=None) < 1.:
+            raise ValueError("Minimum cost to move must be 1, but got %f" % (
+                temp_weight.min(axis=None)))
+        # Ensure start is within bounds.
+        if (temp_start[0] < 0 or temp_start[0] >= temp_weight.shape[0] or
+                temp_start[1] < 0 or temp_start[1] >= temp_weight.shape[1]):
+            raise ValueError("Start lies outside grid.")
+        # Ensure goal is within bounds.
+        if (temp_goal[0] < 0 or temp_goal[0] >= temp_weight.shape[0] or
+                temp_goal[1] < 0 or temp_goal[1] >= temp_weight.shape[1]):
+            raise ValueError("Goal of lies outside grid.")
+
+        height, width = temp_weight.shape
+        start_idx = np.ravel_multi_index(temp_start, (height, width))
+        goal_idx = np.ravel_multi_index(temp_goal, (height, width))
+
+        path = astar(
+            temp_weight.flatten(), height, width, start_idx, goal_idx, allow_diagonal,
+        )
+        return path
+
+    def plot_env(self):
+        plt.cla()
+        plt.imshow(self.op_map, cmap='gray')
+        plt.axis((0, self.map_size[1], self.map_size[0], 0))
+        plt.plot(self.xPoint, self.yPoint, 'b', linewidth=2)
+        plt.plot(self.x2frontier, self.y2frontier, 'r', linewidth=2)
+        plt.plot(self.robot_position[0], self.robot_position[1], 'mo', markersize=8)
+        plt.plot(self.xPoint[0], self.yPoint[0], 'co', markersize=8)
+        plt.pause(0.05)
